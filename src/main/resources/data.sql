ed88de3af472ec89d7d04431b56a90fb0988e7cb
rrgayer <zujpyP-wapxuk-ficgy4>
2020-05-06 23:27:10 -0300

    RestAPI + SQL

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,11 @@
-insert into produto (cd_produto, ds_produto, vr_unitproduto) values (100,"iPhone 5",2400.00);
-insert into produto (cd_produto, ds_produto, vr_unitproduto) values (110,"iPhone 6",2800.00);
-insert into produto (cd_produto, ds_produto, vr_unitproduto) values (120,"iPhone 7",3400.00);
-insert into produto (cd_produto, ds_produto, vr_unitproduto) values (130,"iPhone 8",5200.00);
-insert into produto (cd_produto, ds_produto, vr_unitproduto) values (140,"iPhone X",6700.00);
-insert into produto (cd_produto, ds_produto, vr_unitproduto) values (150,"iPhone 11",8900.00);
+insert into produto (cd_produto, ds_produto, vr_unitproduto) values (100,'iPhone 5',2400.00);
+insert into produto (cd_produto, ds_produto, vr_unitproduto) values (110,'iPhone 6',2800.00);
+insert into produto (cd_produto, ds_produto, vr_unitproduto) values (120,'iPhone 7',3400.00);
+insert into produto (cd_produto, ds_produto, vr_unitproduto) values (130,'iPhone 8',5200.00);
+insert into produto (cd_produto, ds_produto, vr_unitproduto) values (140,'iPhone X',6700.00);
+insert into produto (cd_produto, ds_produto, vr_unitproduto) values (150,'iPhone 11',8900.00);
 
+insert into produto (cd_produto, ds_produto, vr_unitproduto) values (210,'iPad Air',4800.00);
+insert into produto (cd_produto, ds_produto, vr_unitproduto) values (220,'iPad Pro 11',7300.00);
+insert into produto (cd_produto, ds_produto, vr_unitproduto) values (230,'iPad Pro 12',8900.00);
+insert into produto (cd_produto, ds_produto, vr_unitproduto) values (240,'iPad Mini',5900.00);
